d73c6322370abed989c8ac646424da0a0e0466b0
Геннадий <genabox78@gmail.com>
2021-12-11 20:07:56 +0300

    ДЗ выполнено

---
+++ src/test/java/Tests/HW_CSS.java
@@ -0,0 +1,12 @@
+package Tests;
+
+import org.openqa.selenium.By;
+
+public class HW_CSS {
+
+    //1. перейти по ссылке https://calc.by/building-calculators/laminate.html
+
+    //By.cssSelector(".calc-input"); Поиск по классу
+
+
+}
