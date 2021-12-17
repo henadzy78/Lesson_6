c04ba6d435575b8db5d9094c84fe4abea380f651
Геннадий <genabox78@gmail.com>
2021-12-17 10:21:14 +0300

    xPath лекция с выполненным ДЗ-готово.

--- src/test/java/Tests/xPathSelectors.java
+++ src/test/java/Tests/xPathSelectors.java
@@ -35,7 +35,7 @@ public class xPathSelectors {
     private static String searchByStartsWithPath = "//input[starts-with(@type, 'hi')]";
 
     //11. Использование логического оператора
-    private static String searchByTwoAttrPath = "//*[@class=\"form-control \" and @type='text']";
+    private static String searchByTwoAttrPath = "//*[@class='form-control' and @type='text']";
 
     //12. Использование логического оператора
     private static String searchBySpecificAttrPath = "//input[@type != 'hidden']";
@@ -65,6 +65,68 @@ public class xPathSelectors {
 
     //19. Использование preceding-sibling - Выбирает все узлы одного уровня до текущего узла
     private static String path7_1 = "//label[@for=\"announcement\"]/preceding-sibling::div";
+
+
+
+
+    //1.
+    private static String absolutePath1 = "/html/body/div";
+    //2. Все элементы на странице начиная с html-тэга
+    private static String allElementsInHTMLPath1 = "//*";
+    //3. Элемент по тэгу
+    private static String bodyPath1 = "//body";
+    //4. Дочерний элемент относительно родителя
+    private static String childPath1 = "//div/a";
+    //5. Поиск элемента с аттрибутом и его значением
+    private static String tagWithAttrPath1 = "//span[@class='progress-text']";
+    //6. Поиск родителя у элемента с аттрибутом и его значением
+    private static String parentPath11 = "//span[@class='progress-text']/..";
+    private static String parentPath21 = "//*[@class='hamburger hamburger--slider']/parent::div";
+    //7. Поиск элемента по индексу
+    private static String elementByIndexPath1 = "(//div)[3]";
+    //8. Поиск элемента по полному соответствию по тексту
+    private static String searchByTextPath11 = "//*[text()= 'Популярные записи']";
+    private static String searchByTextPath12 = "//*[. = 'Популярные записи']";
+    //9. Поиск элемента по подстроке в тексте
+    private static String searchBySubstringPath1 = "//*[contains(text(), 'Популярные')]";
+    //10. Поиск элемента по аттрибуту значение которого начинается с
+    private static String searchByStartsWithPath1 = "//input[starts-with(@type, 'but')]";
+    //11. Использование логического оператора
+    private static String searchByTwoAttrPath1 = "//*[@rel='nofollow' and @class='inactive']";
+    //12. Использование логического оператора
+    private static String searchBySpecificAttrPath1 = "//*[@href='#popular-posts' and @class='selected' and text()!='Популярные записи']";
+    //13. Использование ancestor - все предки текущего узла
+
+
+    //14. Использование child - все потомки текущего узла зависят от уровня
+    private static String path2_11 = "//*[@id='content_box']/child::div";
+    private static String path2_21 = "//ul/span";           //аналог только проще
+
+    //15. Использование descendant - все потомки текущего узла не зависимо от уровня
+    private static String path3_11= "//*[@id='content_box']//descendant::span";
+    private static String path3_21 = "//ul//span";           //аналог только проще
+
+    //16. Использование following - Выбирает всё в документе после закрытия тэга текущего узла
+    private static String path4_11 = "//*[@class='hamburger hamburger--slider']/following::div";
+    private static String path4_21 = "//*[@class='hamburger hamburger--slider']/following::div/*[@id='wprmenu_menu_ul']";
+
+    //17. Использование following-sibling - Выбирает все узлы одного уровня после текущего узла
+    private static String path5_11 = "//*[@class='hamburger hamburger--slider']/following-sibling::div";
+
+    //18. Использование preceding - Выбирает все узлы, которые появляются перед текущим узлом в документе
+    private static String path6_11 = "//*[@class='hamburger hamburger--slider']/preceding::span";
+
+    //19. Использование preceding-sibling - Выбирает все узлы одного уровня до текущего узла
+    private static String path7_11 = "//*[@id='wprmenu_bar']/preceding-sibling::div";
+
+
+
+
+
+
+
+
+
 
 
 
