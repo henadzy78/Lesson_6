0823ab7aee20b87b87e2ac5e0fb5a78aed2d4091
Геннадий <genabox78@gmail.com>
2022-01-19 19:38:42 +0300

    Lombok

--- src/test/java/models/User.java
+++ src/test/java/models/User.java
@@ -1,28 +1,10 @@
 package models;
 
+import lombok.*;
+
+@Data
+@Builder
 public class User {
     private String email;
     private String password;
-
-
-    public User() {
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public User setEmail(String email) {
-        this.email = email;
-        return this;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public User setPassword(String password) {
-        this.password = password;
-        return this;
-    }
 }
