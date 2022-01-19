220d914af662588a618a7322399a2e4833a18f30
Геннадий <genabox78@gmail.com>
2022-01-19 14:03:32 +0300

    steps

---
+++ src/test/java/tests/MilestoneTests.java
@@ -0,0 +1,26 @@
+package tests;
+
+import baseEntities.BaseTest;
+import models.Milestone;
+import models.Project;
+import models.User;
+import org.testng.annotations.Test;
+import pages.LoginPage;
+
+public class MilestoneTests extends BaseTest {
+
+    @Test
+    public void test1() {
+        Project project = new Project();
+        User user = new User();
+        Milestone milestone = new Milestone();
+
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.login(user);
+
+        projectSteps.addProject(project);
+        milestoneSteps.createMilestone(project, milestone);
+
+
+    }
+}
