d044508a6c3b363622913f9049d74bda7d174805
Геннадий <genabox78@gmail.com>
2022-01-23 19:16:27 +0300

    Update

--- src/main/java/baseEntities/BaseTest.java
+++ src/main/java/baseEntities/BaseTest.java
@@ -2,18 +2,13 @@ package baseEntities;
 
 import core.BrowsersService;
 import core.ReadProperties;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import io.github.bonigarcia.wdm.config.DriverManagerType;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Listeners;
 import utils.Listener;
 import utils.Waits;
 
-import java.util.concurrent.TimeUnit;
 
 @Listeners(Listener.class)
 public class BaseTest {
