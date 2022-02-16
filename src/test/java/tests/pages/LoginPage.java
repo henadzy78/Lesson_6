5f8def545e75acb7f1d8d51127236d1688b9359a
Геннадий <genabox78@gmail.com>
2022-02-16 19:55:11 +0300

    ci_cd-лекция

--- src/test/java/tests/pages/LoginPage.java
+++ src/test/java/tests/pages/LoginPage.java
@@ -1,22 +1,56 @@
 package tests.pages;
 
-import com.codeborne.selenide.SelenideElement;
+import baseEntity.BasePage;
+import models.User;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
-import static com.codeborne.selenide.Selenide.$;
+public class LoginPage extends BasePage {
+    private static final By PAGE_OPENED_IDENTIFIER = By.id("button_primary");
+    private static String ENDPOINT = "/index.php?/auth/login";
+    protected By emailSelector = By.id("name");
+    protected By passwordSelector = By.id("password");
+    protected By loginSelector = By.id("button_primary");
 
-public class LoginPage {
-    private final By username_selector = By.id("name");
-    private final String password_selector = "#password";
-    private final String login_button_selector = "#button_primary";
-
-    public SelenideElement getUsernameField() {
-        return $(username_selector);
+    public LoginPage(WebDriver driver) {
+        super(driver, true);
     }
-    public SelenideElement getPasswordField() {
-        return $(password_selector);
+
+    @Override
+    protected void openPage() {
+        driver.get(BASE_URL + ENDPOINT);
     }
-    public SelenideElement getLoginButton() {
-        return $(login_button_selector);
+
+    @Override
+    protected boolean isPageOpened() {
+        return waits.waitForVisibility(PAGE_OPENED_IDENTIFIER).isDisplayed();
+    }
+
+    public WebElement getEmailField() {
+        return driver.findElement(emailSelector);
+    }
+
+    public WebElement getPasswordField() {
+        return driver.findElement(passwordSelector);
+    }
+
+    public WebElement getLoginButton() {
+        return driver.findElement(loginSelector);
+    }
+
+    public void login(User user) {
+        getEmailField().sendKeys(user.getEmail());
+        getPasswordField().sendKeys(user.getPassword());
+        getLoginButton().click();
+    }
+
+    public void login(String username, String password) {
+        User user = User.builder()
+                .email(username)
+                .password(password)
+                .build();
+
+        login(user);
     }
 }
