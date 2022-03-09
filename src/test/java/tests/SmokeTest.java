a8aafc7dfcf558831014f2dfbd4982e992a1aa19
Геннадий <genabox78@gmail.com>
2022-03-09 20:47:20 +0300

    ValueOfObjects-Update Randomization

--- src/test/java/tests/SmokeTest.java
+++ src/test/java/tests/SmokeTest.java
@@ -1,15 +1,19 @@
 package tests;
 
-import models.User;
-import pages.DashboardPage;
-import pages.LoginPage;
 import baseEntities.BaseTest;
 import core.ReadProperties;
+import models.Project;
+import models.User;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import pages.DashboardPage;
+import pages.LoginPage;
+import utils.Randomization;
 import utils.Retry;
 
 public class SmokeTest extends BaseTest {
+    Project addProject;
+    Project updateProject;
 
     @Test
     public void loginTest() {
@@ -33,10 +37,20 @@ public class SmokeTest extends BaseTest {
         loginPage.getLoginButton().click();
 
         DashboardPage dashboardPage = new DashboardPage(driver);
-        driver.get("https://1511.testrail.io/index.php?/admin/overview");
+        driver.get("https://qa1504.testrail.io/index.php?/admin/overview");
 
         dashboardPage = new DashboardPage(driver, true);
-
         Assert.assertTrue(dashboardPage.getAddProjectButton().isDisplayed());
+    }
+
+
+    private void setupProjects() {
+        addProject = new Project();
+        addProject.setName(Randomization.getRandomString(8));
+        addProject.setTypeOfProject(Randomization.getRandomType());
+
+        updateProject = new Project();
+        updateProject.setName(Randomization.getRandomString(8));
+        updateProject.setTypeOfProject(Randomization.getRandomType());
     }
 }
