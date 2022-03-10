c776da0690821aca260e0adc30af06313037b0c6
Геннадий <genabox78@gmail.com>
2022-03-10 16:15:41 +0300

    Steps

--- src/test/java/baseEntities/BaseTest.java
+++ src/test/java/baseEntities/BaseTest.java
@@ -4,6 +4,8 @@ import core.BrowsersService;
 import core.ReadProperties;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.*;
+import steps.MilestoneSteps;
+import steps.ProjectSteps;
 import utils.Listener;
 import utils.Waits;
 
@@ -13,12 +15,16 @@ public class BaseTest {
     protected WebDriver driver;
     protected BrowsersService browsersService;
     protected Waits waits;
+    protected ProjectSteps projectSteps;
+    protected MilestoneSteps milestoneSteps;
 
     @BeforeClass
     public void setUp() {
         browsersService = new BrowsersService();
         driver = browsersService.getDriver();
         waits = new Waits(driver);
+        projectSteps = new ProjectSteps(driver);
+        milestoneSteps = new MilestoneSteps(driver);
 
         driver.get(ReadProperties.getUrl());
     }
---
+++ src/test/java/steps/ProjectSteps.java
@@ -0,0 +1,24 @@
+package steps;
+
+import baseEntities.BaseStep;
+import models.Project;
+import org.openqa.selenium.WebDriver;
+
+public class ProjectSteps extends BaseStep {
+
+    public ProjectSteps(WebDriver driver) {
+        super(driver);
+    }
+
+    public void addProject(Project project){
+
+    }
+
+    public void updateProject(Project currentProject, Project newProject){
+
+    }
+
+    public void deleteProject(Project project){
+
+    }
+}
